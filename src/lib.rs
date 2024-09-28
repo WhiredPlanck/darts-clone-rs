cf9bbc3930b03eed52d73749590cba5b1d72c20a
WhiredPlanck <whiredplanck@outlook.com>
2024-09-29 00:11:49 +0800

    Update crate meta document

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,70 @@
-//! [Darts-clone](https://github.com/s-yata/darts-clone) is a clone of Darts (Double-ARray Trie System) which is a C++ header library for a static double-array trie structure.
-//! And here is the Rust binding for it.
+//! Rust binding of [Darts-clone](https://github.com/s-yata/darts-clone) - a clone of Darts (Double-ARray Trie System)
+//! 
+//! # Installation
+//！
+//! ```toml
+//! [dependencies]
+//! darts-clone-rs = "0.1.0"
+//! ```
 //!
-//! The features of Darts-clone are as follows:
+//! # Examples
 //!
-//! * Half-size elements
-//!   * Darts-clone uses 32-bit elements and Darts uses 64-bit elements. This feature simply halves the size of dictionaries.
-//! * Directed Acyclic Word Graph (DAWG)
-//!   * Darts uses a basic trie to implement a dictionary. On the other hand, Darts-clone uses a Directed Acyclic Word Graph (DAWG) which is derived from a basic trie by merging its common subtrees. Darts-clone thus requires less elements than Darts if a given keyset contains many duplicate values.
+//! ## Build trie
 //!
-//! Due to these features, Darts-clone can achieve better space efficiency without degrading the search performance.
+//! ```rust
+//! use darts::DartsArrayTrie;
+//! ...
+//! let dic = DartsArrayTrie::new();
+//! let keys: Vec<String> = ... // get keys somehow
+//! let values: Vec<usize> = ... // get values somehow
+//! let lengths: Vec<i32> = ... // get lengths somehow
+//!
+//! let result = dic.build(keys.len, &keys, None /* Some(&values) */, None /* Some(&lengths) */, None);
+//! assert_eq!(Ok(()), result);
+//! ...
+//! ```
+//!
+//! ## Save and open
+//!
+//! ```rust
+//! use darts::DartsArrayTrie;
+//! ...
+//! let dic = DartsArrayTrie::new();
+//! // build ...
+//! let dic_copy = DartsArrayTrie::new();
+//! assert_eq!(Ok(), dic.save("path/to/dict", "wb", 0));
+//! assert_eq!(Ok(), dic_copy.open("path/to/dict", "rb", 0, 0));
+//! ...
+//! ```
+//!
+//! ## Search
+//!
+//! ```rust
+//! use darts::DartsArrayTrie;
+//! ...
+//! const MAX_RESULT_NUM: usize = 16;
+//! let dic = DartsArrayTrie::new();
+//! // build ...
+//! let result = dic.common_prefix_search(key, MAX_RESULT_NUM, 0, 0);
+//! assert_eq!(/* prefixes */, result);
+//! ...
+//! ```
+//!
+//! ## Traverse
+//!
+//! ```rust
+//! use darts::DartsArrayTrie;
+//! ...
+//! let dic = DartsArrayTrie::new();
+//! // build ...
+//! let mut id = 0usize;
+//! let mut key_pos = 0usize;
+//! for i in 0..key.len() {
+//!     let result = dic.traverse(key, &mut id, &mut key_pos, i + 1);
+//!     assert_ne!(result, -2);
+//! }
+//! ...
+//! ```
 
 pub mod darts;
 
